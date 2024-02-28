536256dacc3cc460f9b2ca79d355b5eae352712e
Mahdi-Pranto <mahdiprantoblog@gmail.com>
2024-02-28 23:52:05 +0600

    creating database for the app sqlflite

---
+++ lib/database/sqlite_helper.dart
@@ -0,0 +1,24 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLHelper {
+  //for creating database table
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE items(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        description TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      """);
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'dailynote.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+}
