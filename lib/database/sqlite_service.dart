0f0aeb94ea105532a54608752d5f547bc89d6348
Mahdi-Pranto <mahdiprantoblog@gmail.com>
2024-03-03 19:59:32 +0600

    sqlflite workingg

--- lib/database/sqlite_service.dart
+++ lib/database/sqlite_service.dart
@@ -68,34 +68,3 @@ class DatabaseHelper {
     return await db.delete(_tableName, where: '$colId = ?', whereArgs: [id]);
   }
 }
-
-// Note model class
-class Note {
-  final int id;
-  final String title;
-  final String content;
-  final String createdAt;
-
-  Note(
-      {this.id,
-      required this.title,
-      required this.content,
-      required this.createdAt});
-
-  Map<String, dynamic> toMap() {
-    return {
-      DatabaseHelper.colTitle: title,
-      DatabaseHelper.colContent: content,
-      DatabaseHelper.colCreatedAt: createdAt,
-    };
-  }
-
-  factory Note.fromMap(Map<String, dynamic> map) {
-    return Note(
-      id: map[DatabaseHelper.colId],
-      title: map[DatabaseHelper.colTitle],
-      content: map[DatabaseHelper.colContent],
-      createdAt: map[DatabaseHelper.colCreatedAt],
-    );
-  }
-}
