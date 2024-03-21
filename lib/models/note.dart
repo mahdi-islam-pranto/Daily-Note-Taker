6975018700c7d86b6155d8fb27437f980acc9ab6
Mahdi-Pranto <mahdiprantoblog@gmail.com>
2024-03-21 23:15:46 +0600

    adding sqlflite database for notes

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,32 +1,11 @@
-// // Note model class
-// import 'package:daily_note/database/sqlite_service.dart';
+class Dog {
+  final int id;
+  final String name;
+  final int age;
 
-// class Note {
-//   final int id;
-//   final String title;
-//   final String content;
-//   final String createdAt;
-
-//   Note(
-//       {required this.id,
-//       required this.title,
-//       required this.content,
-//       required this.createdAt});
-
-//   Map<String, dynamic> toMap() {
-//     return {
-//       DatabaseHelper.colTitle: title,
-//       DatabaseHelper.colContent: content,
-//       DatabaseHelper.colCreatedAt: createdAt,
-//     };
-//   }
-
-//   factory Note.fromMap(Map<String, dynamic> map) {
-//     return Note(
-//       id: map[DatabaseHelper.colId],
-//       title: map[DatabaseHelper.colTitle],
-//       content: map[DatabaseHelper.colContent],
-//       createdAt: map[DatabaseHelper.colCreatedAt],
-//     );
-//   }
-// }
+  const Dog({
+    required this.id,
+    required this.name,
+    required this.age,
+  });
+}
